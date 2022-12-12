2eb9e1c1d99f5c898295d506d8c457ad0529c422
andreas riedmann <andreas@riedmann.rocks>
2022-12-12 12:23:01 +0100

    added dotenv

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+require('dotenv').config();
+
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT
+
 app.use(express.json());
 
 const type = ["guitar","piano","electric"]
@@ -15,5 +18,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Server Started at ${443}`)
+    console.log(`Server Started at ${port}`)
 })
