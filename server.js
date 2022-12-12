d407475bde4ce27881530a741f1054b259535d74
andreas riedmann <andreas@riedmann.rocks>
2022-12-12 12:07:53 +0100

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+const type = ["guitar","piano","electric"]
+
+app.get('/', (req, res) => {
+    const data = [];
+    for (let index = 0; index < 20; index++) {
+        let num = Math.floor(Math.random()*1000);
+        data.push({id:index,title:"Hans-" + num,type:type[num%3], description:"Lorem fjkas kdas fal fdjal"});
+    }
+    res.send(data);
+})
+
+app.listen(3002, () => {
+    console.log(`Server Started at ${3000}`)
+})
